a5c0878a962c31612b10ff1fc896202950bc344d
Nuttapol Phomthon <sevipsagis@gmail.com>
2024-08-26 10:21:19 +0700

    add: tailwind, framer-motion, shadcn ui + magic ui setup, tanstack router and million lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from "node:path";
+import MillionLint from "@million/lint";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [MillionLint.vite(), TanStackRouterVite(), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src")
+		}
+	}
 });
